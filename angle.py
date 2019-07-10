ea6b2cbc0a6e9984e70c92a9d9981496adb12419
vitor pavani <vitorpavani@poli.ufrj.br>
2019-07-10 17:42:29 -0300

    add a functionality to reset angles array

--- angle.py
+++ angle.py
@@ -1,21 +1,27 @@
-from fog import Fog
+from fog import Fog, FogException
 import numpy as np
+import keyboard
 
 def main():
     fog = Fog('/dev/ttyS0')
     angles = np.array([])
-    filter_value = 0.0
+    filter_value = 0.1
     while True:
         try:
             angle = fog.get_sample()
             if abs(fog.get_sample()) < filter_value:
                 angle = 0
             angles = np.append(angles,angle)
+            # press z to clear angles list
+            if keyboard.is_pressed('z'):
+                angles = np.array([])
         except KeyboardInterrupt:
             break
         except FogException:
-            print 'there is a problem with Fog.'
+            print "Houston, we've had a problem."
+            
+        
         head_angle = np.trapz(angles,dx=2)
         print "heading angle: " + str(head_angle)        
-
+            
 if __name__=='__main__' : main()
